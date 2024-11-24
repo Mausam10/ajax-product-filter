b6017218bb479a98c5afe840b39b1fb26270d161
Mausam Pariyar <mausam.2020@lagrandee.edu.np>
2024-11-24 14:31:32 +0545

    first commit

---
+++ ajax-product-filter.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: AJAX Product Filter for WooCommerce (Sidebar Edition)
+ * Description: Advanced AJAX product filter with sidebar and checkboxes for WooCommerce.
+ * Version: 1.1
+ * Author: m100
+ */
+
+if (!defined('ABSPATH')) exit;
+
+// Include necessary files
+require_once plugin_dir_path(__FILE__) . 'includes/class-apf-ajax-handler.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-apf-shortcode.php';
+
+// Enqueue styles and scripts
+function apf_enqueue_assets() {
+    if (is_shop() || is_product_category()) {
+        wp_enqueue_style('apf-style', plugin_dir_url(__FILE__) . 'assets/css/style.css');
+        wp_enqueue_script('apf-script', plugin_dir_url(__FILE__) . 'assets/js/ajax-product-filter.js', ['jquery'], '1.0', true);
+        wp_localize_script('apf-script', 'apf_ajax', [
+            'ajax_url' => admin_url('admin-ajax.php'),
+        ]);
+    }
+}
+add_action('wp_enqueue_scripts', 'apf_enqueue_assets');
